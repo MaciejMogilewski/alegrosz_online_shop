8cc90371708e95a10699e6d2d4b3d1579c07e933
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-06-03 09:59:02 +0200

    Add category and subsactegory endpoint, get data from these endpoint and pass to product from state

--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -10,8 +10,50 @@ import {
     TextField,
 } from '@mui/material';
 import DropInput from '../DropInput/DropInput';
+import {useEffect, useState} from "react";
+
+// [{id, category, subcategory: [{id, name}]}]
+
+interface Kind {
+    id: number;
+    name: string;
+}
+
+interface Category extends Kind {
+    subcategory: Kind[];
+}
+
+async function getKind(endpoint: string) {
+    const response = await fetch(`/api/v1/${endpoint}`);
+
+    return response.json();
+}
+
+async function getCategoriesWithSubcategories() {
+
+    const response = await Promise.all([
+        getKind('categories'),
+        getKind('subcategories')]);
+
+    const [categories, subcategories] = response;
+
+    return categories.map((category) => ({
+        ...category,
+        subcategories: category.subcategories
+            .map((subcategoryId) => subcategories
+                .find((subcategory) => subcategory.id === subcategoryId))
+    }))
+}
+
 
 function ProductForm() {
+
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    useEffect(() => {
+        getCategoriesWithSubcategories().then(setCategories);
+    }, [])
+
     const formik = useFormik({
         initialValues: {
             name: '',
